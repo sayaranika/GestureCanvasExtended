547e1a2577d9c322f5ece99569d0b5335c2d4ff6
Anika Sayara <74029806+sayaranika@users.noreply.github.com>
2023-03-18 14:32:09 -0700

    Added Start Scene

---
+++ GestureCanvasExtended/Assets/Script/SceneLauncher.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLauncher : MonoBehaviour
+{
+    public void Launch(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
